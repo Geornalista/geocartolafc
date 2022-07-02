8e2ab10734de5cd4df2c324f60d3578821aa4d5e
Geornalista <83824843+Geornalista@users.noreply.github.com>
2022-07-02 10:44:44 -0300

    Update cartola.py

--- cartola.py
+++ cartola.py
@@ -1,25 +1,18 @@
 import streamlit as st
-import altair as alt
+#import altair as alt
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
+import plotly.graph_objs as go
 
+times = ['América-MG','Athlético-PR','Atlético-GO','Atlético-MG','Avaí','Botafogo',
+        'Bragantino','Ceará','Corinthians','Coritiba','Cuiabá','Flamengo',
+        'Fluminense','Fortaleza','Goiás','Internacional','Juventude',
+        'Palmeiras','Santos','São Paulo']
 
-times = ['América-MG','Athlético-PR','Atlético-GO','Atlético-MG','Bahia','Bragantino','Ceará',
-        'Chapecoense','Corinthians','Cuiabá','Flamengo','Fluminense','Fortaleza','Grêmio',
-        'Internacional','Juventude','Palmeiras','Santos','Sport','São Paulo']
-
-posicoes = ['Goleiro','Zagueiro','Lateral','Meia','Atacante']
-
-rodada_atual = 38
-
-rods = []
-for i in range(rodada_atual):
-  rods.append(i+1)
+posicoes = ['Goleiro','Lateral','Zagueiro','Meia','Atacante','Técnico']
 
 st.sidebar.write(
     """
-    **CARTOLAFC **
+    **CARTOLAFC 2022 **
 
     """
 )
@@ -31,61 +24,46 @@ radio = st.sidebar.radio('Escolha uma posição:',posicoes)
 
 filename = 'cartola.csv'
 cartola = pd.read_csv(filename)
-cartola.drop('Unnamed: 0',axis=1,inplace=True)
+cartola.drop('Unnamed: 0',axis=1, inplace=True)
 
 if len(dropdown1) > 0:
 
-  df = cartola.query('TIME == "{}" & POSICAO == "{}"'.format(dropdown1,radio))
+  df = cartola.query('ADVERSARIO == "{}" & POSICAO == "{}"'.format(dropdown1,radio))
+  df1 = df[['NOME', 'PONTUACAO']]
+  df1.sort_values(by='PONTUACAO',inplace=True)
+  hh = df1.shape[0]
+  if hh > 30:
+    hheight = 1200
+  else:
+    hheight = 600
 
-  df.rename({'G':'GOLS','A':'ASSISTÊNCIA','FT':'FINALIZAÇÃO NA TRAVE',
-          'SG':'JOGO SEM SOFRER GOL','DP':'DEFESA DE PÊNALTI',
-          'DE':'DEFESA','DS':'DESARME','GC':'GOL CONTRA',
-          'CV':'CARTÃO VERMELHO','CA':'CARTÃO AMARELO',
-          'GS':'GOL SOFRIDO','FC':'FALTA COMETIDA',
-          'PC':'PÊNALTI COMETIDO','FD':'FINALIZAÇÃO DEFENDIDA',
-          'FF':'FINALIZAÇÃO PRA FORA','FS':'FALTA SOFRIDA',
-          'PP':'PÊNALTI PERDIDO','I':'IMPEDIMENTO',
-          'PI':'PASSE INCOMPLETO','PS':'PÊNALTI SOFRIDO',
-          }, axis=1, inplace=True)
-  
-  teste = st.checkbox('Selecionar Jogador')
-  
-  if teste:
-    jogadores = df['NOME'].unique()
-    dropdown2 = st.selectbox('Escolha o Jogador', jogadores)
+  fig = go.Figure(
+        data=[go.Bar(x = df1['PONTUACAO'], y = df1['NOME'],
+          name='PONTUACAO',orientation='h',marker_color='green')])
 
-    if len(dropdown2) > 0:
+  fig.update_layout(
+    xaxis_title="Pontos",
+    autosize=False,
+    width=850,
+    height=hheight,
+    margin=dict(
+        l=10,
+        r=10,
+        b=10,
+        t=10,
+        pad=2
+    ),
 
-      df1 = df.query('NOME == "{}"'.format(dropdown2))
+    yaxis=dict(
+        showgrid=True,
+        showline=False,
+        showticklabels=True,
+        zeroline=True,
+    ),
+    paper_bgcolor="white",
+    plot_bgcolor="darkgray",
+    )
 
-      scouts1 = list(df.columns)
-      remove = ['TIME','POSICAO','NOME','RODADA','ADVERSARIO','CASA','FORA']
-      scouts=[]
-      for item in scouts1:
-        if item not in remove:
-          scouts.append(item)
+  st.header('{}: Pontuação enfrentando {}'.format(radio,dropdown1))
 
-      dropdown3 = st.selectbox('Escolha o scout', scouts)
-      
-      pontos=[]
-      for rod in range(rodada_atual):
-        pontos.append(df1[df1['RODADA']==rod+1][dropdown3].sum())
-
-      df2 = pd.DataFrame(
-              {'RODADA':rods,
-               dropdown3:pontos
-               })
-
-      st.header('Time: {} - Posição: {} - Jogador: {}'.format(dropdown1,radio,dropdown2))
-      st.write('TOTAL DE {}'.format(dropdown3))
-
-      bar = alt.Chart(df2, height=400, width=900).mark_bar(size=20).encode(
-        x='RODADA',
-        y=dropdown3,
-        color=alt.value('darkgreen')
-        ).configure_axis(
-          labelFontSize=20,
-          titleFontSize=20
-        )
-
-      st.altair_chart(bar)
+  st.plotly_chart(fig)
