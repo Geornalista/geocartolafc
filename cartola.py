e3a8d57b07e381497fd633194c57dc39b315d143
Geornalista <83824843+Geornalista@users.noreply.github.com>
2023-05-03 11:07:27 -0300

    Update cartola.py

--- cartola.py
+++ cartola.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
 
-times = ['América-MG','Athlético-PR','Atlético-GO','Atlético-MG','Avaí','Botafogo',
-        'Bragantino','Ceará','Corinthians','Coritiba','Cuiabá','Flamengo',
-        'Fluminense','Fortaleza','Goiás','Internacional','Juventude',
-        'Palmeiras','Santos','São Paulo']
+times = ['América-MG','Athlético-PR','Atlético-MG','Bahia','Botafogo',
+        'Bragantino','Corinthians','Coritiba','Cruzeiro','Cuiabá','Flamengo',
+        'Fluminense','Fortaleza','Goiás','Grêmio','Internacional','Palmeiras',
+         'Santos','São Paulo','Vasco']
 
 posicoes = ['Goleiro','Lateral','Zagueiro','Meia','Atacante','Técnico']
 
